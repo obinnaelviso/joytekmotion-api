78dab0fda7bb951c3909f340a1dce6905fcd50a1
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2023-08-16 19:35:01 +0100

    scaffold for page visits

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(
+    [
+        'prefix' => 'v1',
+        'namespace' => 'App\Http\Controllers\Api\\'
+    ],
+    static function () {
+        Route::post('/frontend/page-visit', 'Api\PageVisitController@store')->name('api.page-visit.store');
+        Route::post('/frontend/contact-us', 'Api\PageVisitController@store')->name('api.page-visit.store');
+        Route::post('/frontend/subscribe', 'Api\PageVisitController@store')->name('api.page-visit.store');
+    }
+);
